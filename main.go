bcbcbb7d5af12c41e2e15aaef65a7743c2efba09
Richd0tcom <tuberich0@gmail.com>
2022-11-04 11:47:46 +0100

    feat: add arithmetic endpoint

--- main.go
+++ main.go
@@ -14,6 +14,20 @@ type User struct {
 	Age int32 `json:"age"`
 	Bio string `json:"bio"`
 }
+
+type OperationRequest struct {
+	OperationType string `json:"operation_type"`
+	X int `json:"x"`
+	Y int `json:"y"`
+}
+
+type OperationResponse struct {
+	SlackUsername string `json:"slackUsername"`
+	Result int `json:"result"`
+	OperationType string `json:"operation_type"`
+	
+}
+
 func main(){
 	fmt.Print("Hello, world!")
 
@@ -39,5 +53,38 @@ func main(){
 
 		return fiberContext.JSON(user)
 	})
+
+	app.Post("/operation", func(fiberContext *fiber.Ctx) error {
+		sum := OperationRequest{}
+
+		if err := fiberContext.BodyParser(&sum); err != nil {
+			return err
+		}
+		res:= checkOperation(sum.OperationType, sum.X, sum.Y)
+		result := OperationResponse{
+			SlackUsername: "Richdotcom",
+			OperationType: sum.OperationType,
+			Result: res,
+		}
+		return fiberContext.JSON(result)
+	})
 	log.Fatal(app.Listen(`:`+port))
+}
+
+func checkOperation(opType string, x int, y int) int {
+	var re int
+	switch {
+		case opType == "addition":
+			re= x + y
+			break
+		case opType == "subtraction":
+			re= x - y
+			break
+		case opType == "multiplication":
+			re= x * y
+			break
+
+	}
+
+	return re
 }
